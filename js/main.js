35c3c303f35b33e2069efc150620bfdf7da398a3
Abd-Elrahman Ahmed <git@github.com:mohamed202029999>
2024-07-23 06:29:46 +0300

    upload

---
+++ js/main.js
@@ -0,0 +1,37 @@
+const showContact = document.querySelector('#show')
+const inf = document.querySelector('.inf');
+const navAnc = document.querySelectorAll('#nav ul li a');
+const sections = document.querySelectorAll('section');
+
+
+function header() {
+    //when your are in width more than 1200px add show class to info
+    if (window.innerWidth  > 1200 ) {
+        inf.classList.add('show');
+    } else if (window.innerWidth < 1200) {
+        inf.classList.remove('show');
+        showContact.addEventListener('click', (e) => {
+            inf.classList.toggle('show');
+        });
+    }
+}
+// add (active) class to navigation links and (show) class to its section
+function nvabar(){
+    navAnc.forEach((a, index) => {
+        // when click in any link 
+        a.addEventListener('click', () => {
+            // remove (active) class from all navigation links and add it to the clicked link
+            navAnc.forEach(item => item.classList.remove('active'));
+            a.classList.add('active');
+            // remove (show) class from all sections and add it to the section corresponding to the clicked link
+            sections.forEach(section => section.classList.remove('show'));
+            sections[index].classList.add('show');
+        });
+    });
+}
+
+window.addEventListener('resize', header);
+header();
+nvabar();
+
+
